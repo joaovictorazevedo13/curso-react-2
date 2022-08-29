902a85ade750b2bdbd6da1b77a4f272fbb6ffe87
João Victor Azevedo <109423515+joaovictorazevedo13@users.noreply.github.com>
2022-08-29 15:37:24 -0300

    Att

---
+++ 4_CSS_REACT/css/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MyComponent from './Components/MyComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React com CSS</h1>
+      <MyComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 4_CSS_REACT/css/src/Components/MyComponent.js
@@ -0,0 +1,11 @@
+import "./MyComponent.css";
+
+const MyComponent = () => {
+  return (
+    <div className="MyComponent">
+        <p>CSS de MyComponent</p>
+        </div>
+  )
+}
+
+export default MyComponent
