da4cbf63da05f1b4c8496bd247a34a0cce6451f7
João Victor Azevedo <109423515+joaovictorazevedo13@users.noreply.github.com>
2022-08-29 17:25:01 -0300

    att

--- 4_CSS_REACT/css/src/App.js
+++ 4_CSS_REACT/css/src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
 import MyComponent from './Components/MyComponent';
+import Title from './Components/Title';
+import {useState} from 'react';
 
 function App() {
+
+  const n = 15;
+  const [nome] = useState("João");
+
   return (
     <div className="App">
-      <h1>React com CSS</h1>
+      <Title/>
+      <h3>React com CSS</h3>
       <MyComponent/>
+      <div className='Dinamico'>
+        <h2 style={n < 10 ? ({background: "green"}) : ({background: "red"})}>CSS dinâmico</h2>
+        <h2 style={n > 10 ? ({background: "green"}) : ({background: "red"})}>CSS dinâmico</h2>
+        <h2 style={nome === "João" ? ({background: "aqua"}) : ({background: "purple"})}>CSS dinâmico</h2>
+      </div>
     </div>
   );
 }
